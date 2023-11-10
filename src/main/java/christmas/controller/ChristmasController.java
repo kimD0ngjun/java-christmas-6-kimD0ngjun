ffb8f6373791f74f7cfa9b2e29da6ac52ba317ba
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-10 17:04:52 +0900

    [feat] controller 프로그램 동작 담당 메인 컨트롤러 작성

---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,10 @@
+package christmas.controller;
+
+import christmas.view.InputValue;
+
+public class ChristmasController {
+    public void startEvent() {
+        InputValue.inputDate();
+        InputValue.inputOrder();
+    }
+}
