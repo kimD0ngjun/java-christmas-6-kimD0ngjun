30f2f2bb879d98cbd219d2ea106657e870b87812
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-10 20:05:43 +0900

    [feat] domain 날짜 이벤트 해당 여부 판별 메소드

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -4,8 +4,12 @@ import christmas.message.ErrorMessage;
 
 public class Date {
     // 상수로 날짜 범위 정의
-    private static final int MIN_DATE = 1;
-    private static final int MAX_DATE = 31;
+    private final int MIN_DATE = 1;
+    private final int MAX_DATE = 31;
+    // 크리스마스 디데이 이벤트
+    private final int[] CHRISTMAS_DATES = {1, 2, 8, 9, 15, 16, 22, 23, 29, 30};
+    // 특별 할인 이벤트
+    private final int[] SPECIAL_DISCOUNT = {3, 10, 17, 24, 25, 31};
 
     private final int date;
 
@@ -19,5 +23,25 @@ public class Date {
         if (date < MIN_DATE || date > MAX_DATE) {
             throw new IllegalArgumentException(ErrorMessage.WRONG_DATE.getMessage());
         }
+    }
+
+    // TODO: 크리스마스 디데이 해당 여부
+    private boolean isChristmasDDay() {
+        for (int christmasDate : CHRISTMAS_DATES) {
+            if (date == christmasDate) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // TODO: 특별 할인 해당 여부
+    private boolean isSpecialDiscount() {
+        for (int specialDiscountDate : SPECIAL_DISCOUNT) {
+            if (date == specialDiscountDate ) {
+                return true;
+            }
+        }
+        return false;
     }
 }
