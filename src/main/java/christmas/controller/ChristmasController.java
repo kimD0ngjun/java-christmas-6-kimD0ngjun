3463a5eed95a7a9186d102dd551ab3219eca175e
kimD0ngjun <chickentasty0112@gmail.com>
2023-11-10 19:53:37 +0900

    [feat] 날짜 예외 재입력 단계 기능

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,10 +1,25 @@
 package christmas.controller;
 
+import christmas.domain.Date;
+import christmas.utility.NumberTypeChanger;
 import christmas.view.InputValue;
 
 public class ChristmasController {
     public void startEvent() {
         InputValue.inputDate();
         InputValue.inputOrder();
+    }
+
+    private Date getInputDate() {
+        while (true) {
+            try {
+                String input = InputValue.inputDate();
+                int dateNumber = NumberTypeChanger.changeNumberType(input);
+                Date date = new Date(dateNumber);
+                return date;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
